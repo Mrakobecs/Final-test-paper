a4eb98409e92f483bbf8f61808dba4a022827df7
Mark <mr4ko.bes@yandex.com>
2024-11-03 19:14:15 +0400

    Task

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] initialArray = { "hi", "hello", "yes", "no", "maybe" };
+        int count = 0;
+        for (int i = 0; i < initialArray.Length; i++)
+        {
+            if (initialArray[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+        string[] newArray = new string[count];
+        int index = 0;
+        for (int i = 0; i < initialArray.Length; i++)
+        {
+            if (initialArray[i].Length <= 3)
+            {
+                newArray[index] = initialArray[i];
+                index++;
+            }
+        }
+        Console.WriteLine("Новый массив строк, длина которых <= 3:");
+        foreach (var str in newArray)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
